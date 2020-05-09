3b4e29ea79f61106f2d38a0a66f9d8d0f8a5c246
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-05-09 12:07:37 +0530

    Done with button to roll

--- src/components/Roll.js
+++ src/components/Roll.js
@@ -1,12 +1,41 @@
 import React,{Component} from 'react';
-import {View, Image, Text, StyleSheet} from 'react-native';
+import {View, Image, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+
+
+const s=StyleSheet.create({
+  gb:{
+    fontSize:20,
+    marginTop:100,
+    color:'white',
+    fontWeight:'bold',
+    borderWidth:2,
+    borderRadius:10,
+    paddingVertical:10,
+    paddingHorizontal:10,
+    borderColor:'white',
+    textAlign:'center'
+  }
+});
 
 
 export default class Roll extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.press=this.press.bind(this);
+  }
+
+
+  press=e=>Alert.alert('Make roll function')
+
+
   render(){
     return(
       <View>
       <Image source={require('../images/dice3.png')}/>
+      <TouchableOpacity onPress={this.press}>
+      <Text style={s.gb}>Play Game</Text>
+      </TouchableOpacity>
       </View>
     );
   }
